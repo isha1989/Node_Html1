a3bac9988c3bbf1b0f31f57725a4b6ccf6143b10
isha1989 <ishakalia1322@gmail.com>
2020-02-25 20:20:52 -0600

    Create index59.js

---
+++ JavaScript/index59.js
@@ -0,0 +1,17 @@
+let firstNumber=10;
+let secondNumber=20;
+if(firstNumber==secondNumber)
+{
+    console.log("They are the same number");
+}
+else if(firstNumber>secondNumber)
+{
+    console.log(`${firstNumber} is bigger than ${secondNumber}`);
+}
+else if(secondNumber>firstNumber)
+{
+    console.log(`${secondNumber} is bigger than ${firstNumber}`);
+}
+else{
+    console.log("invalid entry");
+}
