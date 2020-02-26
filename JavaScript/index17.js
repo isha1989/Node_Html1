65c5cb7ab10ae67cf39461e7f3842e016f8cf60c
isha1989 <ishakalia1322@gmail.com>
2020-02-25 18:46:57 -0600

    Create index17.js

---
+++ JavaScript/index17.js
@@ -0,0 +1,11 @@
+let seriesName = "Game of Thrones ";
+let dragon1 = "drogon ";
+let dragon2 = "Rhaegal ";
+let dragon3 = "Viserion ";
+let template = `series name: ${seriesName} the dragon names are ${dragon1} ${dragon2} & ${dragon3}`;
+console.log(template);
+let template2 = "series name: " + seriesName + "the dragon names are" + dragon1 + dragon2 + "&" + dragon3;
+console.log(template2);
+
+
+
