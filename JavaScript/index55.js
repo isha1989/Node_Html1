0334aef99a63d8f0d0e8ed2d8f2f5f3aa4cae7e4
isha1989 <ishakalia1322@gmail.com>
2020-02-25 20:20:33 -0600

    Create index55.js

---
+++ JavaScript/index55.js
@@ -0,0 +1,4 @@
+let firstNumber=10;
+let secondNumber=5;
+let message= firstNumber>secondNumber? firstNumber +" "+ "is bigger than"+" " +secondNumber: firstNumber +" "+ "is less than"+" " +secondNumber;
+console.log(message);
