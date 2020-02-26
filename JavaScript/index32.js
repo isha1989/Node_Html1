85dde87ffe06595b8feb0c418463b2df409f2561
isha1989 <ishakalia1322@gmail.com>
2020-02-25 18:48:22 -0600

    Create index32.js

---
+++ JavaScript/index32.js
@@ -0,0 +1,6 @@
+let height=10;
+let base=4;
+
+let per= 2*(height+base);
+let area= height*base;
+console.log(`rectangle perimeter ${per} and area ${area} with height ${height} and base as ${base}`)
