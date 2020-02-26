a1ba973ff2a05a95890a30850e862d0a96752d5a
isha1989 <ishakalia1322@gmail.com>
2020-02-25 18:49:20 -0600

    Create index42.js

---
+++ JavaScript/index42.js
@@ -0,0 +1,14 @@
+let variable1=`Ready Player One is a 2011 science fiction novel, and the debut novel of American author Ernest Cline. The story, set in a dystopian 2044, follows protagonist Wade Watts on his search for an Easter egg in a worldwide virtual reality game, the discovery of which will lead him to inherit the game creator's fortune. Cline sold the rights to publish the novel in June 2010, in a bidding war to the Crown Publishing Group (a division of Random House)]. The book was published on August 16, 2011. An audiobook was released the same day; it was narrated by Wil Wheaton, who was mentioned briefly in one of the chapters.Ch. 20 In 2012, the book received an Alex Award from the Young Adult Library Services Association division of the American Library Association and won the 2012 Prometheus Award.`;
+console.log(variable1.toUpperCase());
+console.log(variable1.toLowerCase());
+let variable2=`Ready Player One is a 2011 science fiction novel, and the debut novel of American author Ernest Cline. The story, set in a dystopian 2044, follows protagonist Wade Watts on his search for an Easter egg in a worldwide virtual reality game, the discovery of which will lead him to inherit the game creator's fortune.`;
+console.log(variable2.toUpperCase());
+let variable3= `Cline sold the rights to publish the novel in June 2010, in a bidding war to the Crown Publishing Group (a division of Random House)]. The book was published on August 16, 2011. An audiobook was released the same day; it was narrated by Wil Wheaton, who was mentioned briefly in one of the chapters.Ch. 20 In 2012, the book received an Alex Award from the Young Adult Library Services Association division of the American Library Association and won the 2012 Prometheus Award.`;
+console.log(variable3.toLowerCase());
+const l=console.log //creating short cut for console.log()
+l(variable2 +"\n" + variable3);
+const combine=variable2.concat(variable3);
+const len= combine.length;
+l(`The message has ${len} characters long`);
+
+
