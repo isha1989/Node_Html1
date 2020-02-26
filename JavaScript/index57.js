9a6c9f8c6390bc10c220147fb472c4d36df00be9
isha1989 <ishakalia1322@gmail.com>
2020-02-25 20:20:42 -0600

    Create index57.js

---
+++ JavaScript/index57.js
@@ -0,0 +1,4 @@
+let username="pepe2017";
+let password="123456";
+let message= username==="pepe2017" && password==="12345"? "Logged in user, show user home page" : "Sorry, there has been a problem, please try it again.";
+console.log(message);
