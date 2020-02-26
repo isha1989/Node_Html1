32f3a56413ed2ba7bdbd0af201b3e113b737c748
isha1989 <ishakalia1322@gmail.com>
2020-02-25 18:50:15 -0600

    Create index54.js

---
+++ JavaScript/index54.js
@@ -0,0 +1,10 @@
+let username="pepe2017";
+let password="123456";
+if(username==="pepe2017" && password==="12345")
+{
+    console.log("Logged in user, show user home page");
+}
+else
+{
+    console.log("Sorry, there has been a problem, please try it again.");
+}
