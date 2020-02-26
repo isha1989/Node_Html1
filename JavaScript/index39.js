3031fcbba1952394e795b7411e6dadf5d8c2f39f
isha1989 <ishakalia1322@gmail.com>
2020-02-25 18:48:56 -0600

    Create index39.js

---
+++ JavaScript/index39.js
@@ -0,0 +1,10 @@
+const firstName="Isha";
+const lastName="Kalia";
+let fCCount= firstName.length;
+let lCCount=lastName.length;
+let charDiff= lCCount-fCCount;
+console.log("my first name is" + " "+ firstName +" " + "character count is : " + fCCount);
+console.log("my last name is" + " "+ lastName+" " + "character count is : " + lCCount );
+console.log(`The character difference between my first name and last name is ${charDiff}`)
+console.log(fCCount>lCCount);
+
