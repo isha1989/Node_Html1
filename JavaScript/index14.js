851b4566891ea861e1279c1ebf9346feea2af960
isha1989 <ishakalia1322@gmail.com>
2020-02-25 18:46:54 -0600

    Create index14.js

---
+++ JavaScript/index14.js
@@ -0,0 +1,6 @@
+const firstName="Isha";
+const greeting="Welcome";
+const endOfMessage="to learning JavaScript with friends";
+console.log("%greeting% %firstname% %endOfMessage%")
+console.log( greeting +firstName + endOfMessage);
+console.log(`${greeting} ${firstName} ${endOfMessage}`)
