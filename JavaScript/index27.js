9a8b3a5cfa9398e677b43bef6982c69403d03d26
isha1989 <ishakalia1322@gmail.com>
2020-02-25 18:47:15 -0600

    Create index27.js

---
+++ JavaScript/index27.js
@@ -0,0 +1,13 @@
+let firstNumber= 30
+let secondNumber= 50
+let add= firstNumber+secondNumber;
+let subtract= firstNumber-secondNumber;
+let mul=firstNumber*secondNumber
+let div=firstNumber/secondNumber
+let rem=firstNumber%secondNumber
+
+console.log(add);
+console.log(subtract);
+console.log(mul)
+console.log(div);
+console.log(rem);
