3b21a685325e355ca9bd5315049723a5fb9c22b3
isha1989 <ishakalia1322@gmail.com>
2020-02-25 18:49:27 -0600

    Create index44.js

---
+++ JavaScript/index44.js
@@ -0,0 +1,14 @@
+let text="HELLO";
+let result;
+let i;
+let var1="";
+//attempt one
+for(i=text.length-1; i>=0; i--)
+{
+     var1= var1 +text[i];
+    
+
+}
+console.log(var1);
+console.log("final result is" +" " + var1.toLowerCase());
+//attempt 2
