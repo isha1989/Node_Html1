5490b5534a6be43b66f4ed37a9609ad542273475
isha1989 <ishakalia1322@gmail.com>
2020-02-25 18:48:26 -0600

    Create index33.js

---
+++ JavaScript/index33.js
@@ -0,0 +1,15 @@
+
+let userCount=100;
+userCount+=5;
+console.log(`user count ${userCount}`);
+userCount-=3;
+console.log(`user count ${userCount}`);
+userCount*=2;
+console.log(`user count ${userCount}`);
+let mCount, wCount;
+mCount=userCount/2;
+
+
+wCount=userCount/2;
+console.log(`user count ${userCount} , men count ${mCount} and women count ${wCount}`);
+
