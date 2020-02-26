ed99e15822fc8b4cb6adf65bce38177642190478
isha1989 <ishakalia1322@gmail.com>
2020-02-25 18:50:26 -0600

    Create index7.js

---
+++ JavaScript/index7.js
@@ -0,0 +1,12 @@
+let firstName="Isha"
+let lastName="kalia"
+let age= 30
+let address= "1322 Rupert Street"
+let dateOfBirth= "03/15/1989"
+let fullName= firstName + lastName;
+console.log("First Name :" + ""+ firstName)
+console.log("Full Name: " + " " + fullName)
+console.log("Last Name :" +" "+ lastName)
+console.log("Age :"+" "+ age)
+console.log("Address :"+""+ address)
+console.log("Date of Birth :" +""+dateOfBirth)
