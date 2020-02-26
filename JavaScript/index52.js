e427e9d0563bea00341c6960726306414f320b1a
isha1989 <ishakalia1322@gmail.com>
2020-02-25 18:50:06 -0600

    Create index52.js

---
+++ JavaScript/index52.js
@@ -0,0 +1,10 @@
+let firstNumber=10;
+let secondNumber=5;
+if (firstNumber>secondNumber)
+{
+    console.log( `Number ${firstNumber} is bigger than ${secondNumber}`)
+}
+else
+{
+    console.log(`Number ${secondNumber} is bigger than ${firstNumber}`)
+}
