2bb500d7050f0342482df3227ea1aa67b6386f4a
isha1989 <ishakalia1322@gmail.com>
2020-02-25 18:49:32 -0600

    Create index45.js

---
+++ JavaScript/index45.js
@@ -0,0 +1,19 @@
+let playerName= "Darian Durant is lame";
+let team= "BC Lions, Calgary Stampeders, Edmonton Eskimos, Saskatchewan Roughriders & Winnipeg Blue Bombers";
+let message="Regina is the best Canadian city, Go Riders";
+let a= playerName.slice(0,16);
+console.log(a);
+let b=team.slice(66, 73);
+console.log(b);
+let c=message.slice(10, 18);
+console.log(c);
+let d= message.slice(32,36);
+console.log(d);
+let f=b.charAt(0).toUpperCase();
+let g= b.slice(1,6);
+console.log(f+g);
+let e= b.replace("r","R");
+console.log(e);
+console.log(a+" "+c+" "+ e);
+
+
