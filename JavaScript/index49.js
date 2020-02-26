4f6eba6b425866b24f8c96c98dc0e5c1b15247da
isha1989 <ishakalia1322@gmail.com>
2020-02-25 18:49:48 -0600

    Create index49.js

---
+++ JavaScript/index49.js
@@ -0,0 +1,12 @@
+let firstNumber=10;
+let secondNumber=5;
+if (firstNumber>secondNumber)
+{
+    console.log( `Number ${firstNumber} is bigger than ${secondNumber}`)
+}
+let firstNumber1=20;
+let secondNumber1=30;
+if (firstNumber1>secondNumber1)
+{
+    console.log( `Number ${firstNumber1} is bigger than ${secondNumber1}`)
+}
