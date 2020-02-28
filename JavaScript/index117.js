1fb474b9fb2a80385c83409da3e31ce2c935abc5
isha1989 <ishakalia1322@gmail.com>
2020-02-27 21:12:34 -0600

    Create index117.js

---
+++ JavaScript/index117.js
@@ -0,0 +1,7 @@
+let student=["isha", "nishant", "geetanjali", "rachita", "tanya"];
+console.log(student[0]);
+console.log(student[1]);
+console.log(student[2]);
+console.log(student[3]);
+console.log(student[4]);
+
