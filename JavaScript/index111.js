328a80a543e788da096dd31790a837f3a8ad5554
isha1989 <ishakalia1322@gmail.com>
2020-02-27 21:12:07 -0600

    Create index111.js

---
+++ JavaScript/index111.js
@@ -0,0 +1,22 @@
+let a;
+
+function even(a)
+{
+    return a%2=== 0;
+    /*
+if(a%2==0)
+{
+    console.log(`${a} is even number`);
+    return true;
+}
+else
+{
+    console.log(`${a} is odd number`);
+    return false;
+}*/
+}
+even(2);
+even(5);
+even(3);
+even(24);
+even(12);
