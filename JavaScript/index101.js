6ce49c123123731aa6b3ef380ff6dc0a7fee14ac
isha1989 <ishakalia1322@gmail.com>
2020-02-27 21:11:46 -0600

    Create index101.js

---
+++ JavaScript/index101.js
@@ -0,0 +1,13 @@
+
+function showUser()
+{
+    let name="isha ";
+    let age=30 ;
+    let phoneNumber=3062168786 ;
+    let street="Rupert Street ";
+    let postalCode="S4N1V7 ";
+    let married=yes;
+    console.log(name);
+}
+
+showUser();
