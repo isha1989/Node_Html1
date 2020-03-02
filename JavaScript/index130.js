e134f68b07921907075b8638bfe66584c0f8f2f5
isha1989 <ishakalia1322@gmail.com>
2020-03-02 17:21:06 -0600

    Create index130.js

---
+++ JavaScript/index130.js
@@ -0,0 +1,10 @@
+let mutant=["Professor X", "Cyclops", "Iceman", "Angel"];
+let a= [];
+a= mutant.sort();
+console.log(a);
+let b=[];
+b=mutant.reverse();
+console.log(b);
+let c=[];
+c=mutant.join("*");
+console.log(c);
