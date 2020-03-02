b280be2b33613cace5b1199813f68aee409c132e
isha1989 <ishakalia1322@gmail.com>
2020-03-02 17:21:20 -0600

    Create index126.js

---
+++ JavaScript/index126.js
@@ -0,0 +1,7 @@
+let iceCreamFlavours=[];
+let iceCream=iceCreamFlavours.unshift("vanilla");
+console.log(iceCreamFlavours);
+iceCream=iceCreamFlavours.unshift("butterscotch");
+console.log(iceCreamFlavours);
+iceCream=iceCreamFlavours.unshift("chocolate");
+console.log(iceCreamFlavours);
