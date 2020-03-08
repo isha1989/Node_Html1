d50ebbe4b60a423a488a7f5d287a6847d539a747
isha1989 <ishakalia1322@gmail.com>
2020-03-08 17:59:59 -0600

    Create index140.js

---
+++ JavaScript/index140.js
@@ -0,0 +1,7 @@
+const mutants=["Professor",    "Cyclops",  "Iceman",   "Angel",   "Magneto"    ]
+    
+    mutants.forEach(function(m) {
+        console.log(m);
+    
+});
+let temp= mutants.filter
