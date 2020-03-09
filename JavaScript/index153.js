8a7cccfe77ee12ea70735328e500a9c9c97499a1
isha1989 <ishakalia1322@gmail.com>
2020-03-08 18:00:34 -0600

    Create index153.js

---
+++ JavaScript/index153.js
@@ -0,0 +1,33 @@
+let aritmentic={
+    add : function(a, b)
+    {
+        let sum= a+b;
+        console.log(sum);
+    },
+    subtract : function(a, b)
+    {
+        let sub= a-b;
+        console.log(sub);
+    },
+    multiply : function(a, b)
+    {
+        let mul= a*b;
+        console.log(mul);
+    },
+    divide : function(a, b)
+    {
+        let div= a/b;
+        console.log(div);
+    },
+    remainder : function(a, b)
+    {
+        let rem= a%b;
+        console.log(rem);
+    }
+
+};
+aritmentic.add(20,10);
+aritmentic.subtract(20,10);
+aritmentic.multiply(20,10);
+aritmentic.divide(20,10);
+aritmentic.remainder(20,10);
