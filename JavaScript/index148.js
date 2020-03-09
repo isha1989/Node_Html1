eb33cec4243dc2c4ecb1940b5f76d9a07eca3a69
isha1989 <ishakalia1322@gmail.com>
2020-03-08 18:00:21 -0600

    Create index148.js

---
+++ JavaScript/index148.js
@@ -0,0 +1,10 @@
+let superhero ={name: "batman",
+secretName: "Bruce",
+sidekick: "['Robin', 'Alfred', 'Gordon']",strength: "70", 
+};
+console.log(superhero);
+console.log(superhero.speed);
+
+superhero.speed="80";
+console.log(superhero.speed);
+
