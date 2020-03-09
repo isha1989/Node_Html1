9997556ee4a3863cef00462892da4a3f16ad931a
isha1989 <ishakalia1322@gmail.com>
2020-03-08 18:00:54 -0600

    Create index158.js

---
+++ JavaScript/index158.js
@@ -0,0 +1,160 @@
+function showGameDetails(a) {
+    console.log("name: ", a.name);
+    console.log("price: ", a.price);
+    console.log("sold: ", a.sold);
+    console.log("console: ", a.console);
+    
+  }
+
+const games = [
+    {
+      name: "Crash Bandicoot N. Sane Trilogy",
+      price: 1060,
+      sold: 20,
+      console: "PS4",
+      showDetail : function(){
+      showGameDetails(this);
+      }
+    },
+    {
+      name: "Lego Marvel Super Heroes",
+      price: 700,
+      sold: 25,
+      console: "XBOX",
+      showDetail : function(){
+        showGameDetails(this);
+        }
+    },
+    {
+      name: "Gta V",
+      price: 1449,
+      sold: 30,
+      console: "PS4",
+      showDetail : function(){
+        showGameDetails(this);
+        }
+    },
+    {
+      name: "Mortal Kombat Xl",
+      price: 1190,
+      sold: 34,
+      console: "PS4",
+      showDetail : function(){
+        showGameDetails(this);
+        }
+    },
+    {
+      name: "Gta V",
+      price: 1250,
+      sold: 60,
+      console: "XBOX",
+      showDetail : function(){
+        showGameDetails(this);
+        }
+    },
+    {
+      name: "Fifa 2017",
+      price: 890,
+      sold: 15,
+      console: "PS4",
+      showDetail : function(){
+        showGameDetails(this);
+        }
+    },
+    {
+      name: "Uncharted 4",
+      price: 950,
+      sold: 30,
+      console: "PS4",
+      showDetail : function(){
+        showGameDetails(this);
+        }
+    },
+    {
+      name: "Uncharted 4",
+      price: 950,
+      sold: 30,
+      console: "PS4",
+      showDetail : function(){
+        showGameDetails(this);
+        }
+    },
+    {
+      name: "Need For Speed",
+      price: 790,
+      sold: 10,
+      console: "PS4",
+      showDetail : function(){
+        showGameDetails(this);
+        }
+    },
+    {
+      name: "Lego Batman",
+      price: 1000,
+      sold: 18,
+      console: "PS4",
+      showDetail : function(){
+        showGameDetails(this);
+        }
+    },
+    {
+      name: "Fifa 17",
+      price: 1290,
+      sold: 12,
+      console: "PS4",
+      showDetail : function(){
+        showGameDetails(this);
+        }
+    },
+    {
+      name: "Resident Evil 7",
+      price: 1390,
+      sold: 10,
+      console: "PS4",
+      showDetail : function(){
+        showGameDetails(this);
+        }
+    },
+    {
+      name: "Dragon Ball XX",
+      price: 1390,
+      sold: 25,
+      console: "XBOX",
+      showDetail : function(){
+        showGameDetails(this);
+        }
+    },
+  ];
+  let con= "PS4";
+  let PS4Games = games.filter(function(game)
+  {
+    game.console === "PS4"
+  } );
+  let budget = 0;
+  PS4Games.forEach(function(game)
+  { budget = budget + game.price;}
+  );
+  console.log(`I need ${budget} CAD to get all this PS4 great games!!`);
+  let gameNames= PS4Games.map(function(game)
+  {
+     return game.name;
+  });
+  console.log(gameNames);
+
+  let gamePrices= PS4Games.map(function(game)
+  {
+    return game.price;
+    
+  });
+  console.log(gamePrices);
+  
+  let gamesNew= games.map(function(game)
+  {
+   return game.name;
+  });
+  games.forEach(function(game) {
+      if(game.console== con)
+      {console.log(game.showDetail());}
+      
+    
+  });
