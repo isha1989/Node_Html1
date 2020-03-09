22fd8d4aadd3589db74385dd1f8e1d11c370a080
isha1989 <ishakalia1322@gmail.com>
2020-03-08 18:00:59 -0600

    Create index159.js

---
+++ JavaScript/index159.js
@@ -0,0 +1,16 @@
+/*let jeep={
+    brand : "Jeep Wrangler",
+    price : 34000,
+    showData: function()
+    {
+        console.log(`The ${this.brand} is ${this.price}`);
+        
+    }
+
+};
+jeep.showData();*/
+/* Another way */
+let jeep1={};
+jeep1.brand1="jeep wrangler";
+jeep1.price1= 34000;
+console.log(`The ${jeep1.brand1} is ${jeep1.price1}`);
