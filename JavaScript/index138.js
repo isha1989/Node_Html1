4c7902dfcb80872d9026bc634d21708a99b8d278
isha1989 <ishakalia1322@gmail.com>
2020-03-08 18:00:08 -0600

    Create index138.js

---
+++ JavaScript/index138.js
@@ -0,0 +1,11 @@
+
+const mutants = ["Professor X", "Cyclops", "Iceman", "Angel", "Magneto", "Logan", "Pheonix", "Gambit"];
+mutants.map(function (m)
+{
+if(m=="Professor X" || m=="Logan" || m=="Pheonix" || m=="Gambit")
+{
+    console.log(m);
+}
+}
+);
+
