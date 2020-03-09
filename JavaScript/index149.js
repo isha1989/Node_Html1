aad21c1253eeb0fe56e5146e09c5f913a35e5477
isha1989 <ishakalia1322@gmail.com>
2020-03-08 18:00:26 -0600

    Create index149.js

---
+++ JavaScript/index149.js
@@ -0,0 +1,13 @@
+let user={name: "isha",
+age: "30",
+gender: "female",
+address: "123",
+city: "regina",
+
+};
+user.income="$1";
+user.married="true";
+user.children="1";
+user.job="Caseworker";
+user.ethinicity="Indian";
+console.log(user);
